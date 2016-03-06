9fc5e2c0b54ea7ae8f88468923f2bb6081f2d7d1
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2016-03-06 22:00:13 +0100

    starting the mountain

--- src/Character.hx
+++ src/Character.hx
@@ -15,7 +15,8 @@ class Character extends Entity {
   private var angleMove:AngleTween;
   private var boatAngle:Float;
   private var rowRadius:Int;
-  private var boatSpeed:Int;
+  private var boatSpeed:Float;
+  private var turnSpeed:Float;
 
   private var velX:Float;
   private var velY:Float;
@@ -24,110 +25,44 @@ class Character extends Entity {
     super(x, y);
 
     sprite = new Image('graphics/boat.png');
-    sprite.scale = 3.0;
+    sprite.scale = 4.0;
     sprite.smooth = false;
     sprite.centerOrigin();
     graphic = sprite;
 
-    boatAngle = 0;
+    boatAngle = Math.PI / 2;
 
     velX = 0;
     velY = 0;
 
     rowRadius = 5;
-    boatSpeed = 10;
-
-    setupTweens();
-  }
-
-  public function setupTweens () {
-    motion = new LinearMotion();
-    angleMove = new AngleTween(TweenType.Persist);
-
-    this.addTween(motion);
-    this.addTween(angleMove);
-  }
-
-  private function tweenDone(_):Void {
-    // done;
-    return;
-  }
-
-  /**
-   * a
-   * |\
-   * | \
-   * |  \
-   * |---.
-   * b    c
-   *
-   *  cx = ax + r sin a'
-   *  cy = ay - r(1 - cos a')
-   */
-   private function calculateCenterOfMotion (originX:Float, originY:Float, radius, startAngle:Float, ?left:Bool=false) {
-    var centerOfMotion = new Map<String, Int>();
-    var direction = 1;
-    var angle = HXP.RAD * startAngle + 6.283185307 / 10;
-    var x = originX + radius * Math.sin(angle);
-    if (left) {
-      direction = -1;
-    }
-    var y = originY - radius * (direction - Math.cos(angle));
-
-    centerOfMotion.set('x', Std.int(x));
-    centerOfMotion.set('y', Std.int(y));
-    return centerOfMotion;
-  }
-
-  private function calculateNextPoint(originX:Float, originY:Float, angle:Float, speed:Int) {
-    var newLocation = new Map<String, Int>();
-
-    var dX = speed * Math.cos(angle * HXP.RAD);
-    var dY = speed * Math.cos(angle * HXP.RAD);
-    trace(dX, dY);
-    newLocation.set('x', Std.int(x - dX));
-    newLocation.set('y', Std.int(y - dY));
-    return newLocation;
-  }
-
-  public function goRight () {
-    angleMove.tween(boatAngle, boatAngle - 45.0, 0.3);
-    angleMove.start();
-    // var c = calculateCenterOfMotion(x, y, rowRadius, boatAngle);
-    // motion.setMotion(c.get('x'), c.get('y'), rowRadius, boatAngle, true, 0.3, Ease.circIn);
-    var p = calculateNextPoint(x, y, boatAngle + 45, boatSpeed);
-    motion.setMotion(x, y, p.get('x'), p.get('y'), 0.3);
-    motion.start();
-  }
-
-  public function goLeft () {
-    angleMove.tween(boatAngle, boatAngle + 45.0, 0.3);
-    angleMove.start();
-    var p = calculateNextPoint(x, y, boatAngle - 45, boatSpeed);
-    motion.setMotion(x, y, p.get('x'), p.get('y'), 0.3);
-
-    // var c = calculateCenterOfMotion(x, y, rowRadius, boatAngle, true);
-    // motion.setMotion(c.get('x'), c.get('y'), rowRadius, boatAngle, false, 0.3, Ease.circIn);
-    motion.start();
-  }
-
-  public function updateAngle(angle) {
-    sprite.angle += angle;
+    boatSpeed = 0.5;
+    turnSpeed = 0.1;
 
   }
+
+  public function goLeft() {
+    velX -= boatSpeed * Math.cos(boatAngle);
+    velY -= boatSpeed * Math.sin(boatAngle);
+    boatAngle = boatAngle + turnSpeed;
+  }
+
+  public function goRight() {
+    velX += boatSpeed * Math.cos(boatAngle);
+    velY += boatSpeed * Math.sin(boatAngle);
+    boatAngle = boatAngle - turnSpeed;
+  }
+
 
   public override function update () {
-    sprite.angle = boatAngle;
+    sprite.angle = 180 - boatAngle * HXP.DEG;
 
-    if (angleMove.active) {
-      boatAngle = angleMove.angle;
-    }
+    velX *= 0.97; // slow down x and y
+    velY *= 0.97;
 
-    if (motion.active) {
-      trace(this.x, motion.x);
-      this.x = motion.x;
-      this.y = motion.y;
-    }
+    x += velX;
+    y += velY;
+
     super.update();
   }
 }
--- src/MainScene.hx
+++ src/MainScene.hx
@@ -2,15 +2,23 @@ import com.haxepunk.Scene;
 import Inputs;
 
 import Character;
+import EmitController;
+import Mountain;
 
 class MainScene extends Scene
 {
-	public var kagiso:Character;
+	private var kagiso:Character;
+	private var WATERFALL_SPEED:Float;
+	private var ec:EmitController;
+	private var mountain:Mountain;
+
 	public override function begin()
 	{
+		WATERFALL_SPEED = 0.3;
 		kagiso = new Character(500, 200);
 		add(kagiso);
-
+		ec = add(new EmitController());
+		mountain = add(new Mountain(10,10));
 	}
 
 	public override function update () {
@@ -18,9 +26,14 @@ class MainScene extends Scene
 
 		if (Inputs.action() == 'right') {
 			kagiso.goRight();
+			// ec.splash(kagiso.x, kagiso.y);
 		}
 		if (Inputs.action() == 'left') {
 			kagiso.goLeft();
+			// ec.splash(kagiso.x, kagiso.y);
 		}
+
+		kagiso.y += WATERFALL_SPEED;
+
 	}
 }
